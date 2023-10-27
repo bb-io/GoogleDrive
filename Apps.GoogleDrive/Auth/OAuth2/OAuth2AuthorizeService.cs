3ed59abbada118e3e3b04c39cb407a27a8dc25be
bohdanm-bb <bohdan.makarenko@blackbird.io>
2023-10-27 18:05:37 +0300

    Bridge oauth

--- Apps.GoogleDrive/Auth/OAuth2/OAuth2AuthorizeService.cs
+++ Apps.GoogleDrive/Auth/OAuth2/OAuth2AuthorizeService.cs
@@ -1,23 +1,32 @@
-﻿using Blackbird.Applications.Sdk.Common.Authentication.OAuth2;
+﻿using Blackbird.Applications.Sdk.Common;
+using Blackbird.Applications.Sdk.Common.Authentication.OAuth2;
+using Blackbird.Applications.Sdk.Common.Invocation;
 using Microsoft.AspNetCore.WebUtilities;
 
 namespace Apps.GoogleDrive.Auth.OAuth2;
 
-public class OAuth2AuthorizeService : IOAuth2AuthorizeService
+public class OAuth2AuthorizeService : BaseInvocable, IOAuth2AuthorizeService
 {
+    public OAuth2AuthorizeService(InvocationContext invocationContext) : base(invocationContext)
+    {
+    }
+
     public string GetAuthorizationUrl(Dictionary<string, string> values)
     {
+        string bridgeOauthUrl = $"{InvocationContext.UriInfo.BridgeServiceUrl.ToString().TrimEnd('/')}/oauth";
         const string oauthUrl = "https://accounts.google.com/o/oauth2/v2/auth";
         var parameters = new Dictionary<string, string>
         {
             { "client_id", ApplicationConstants.ClientId },
-            { "redirect_uri", ApplicationConstants.RedirectUri },
+            { "redirect_uri", $"{InvocationContext.UriInfo.BridgeServiceUrl.ToString().TrimEnd('/')}/AuthorizationCode" },
             { "response_type", "code" },
             { "scope", ApplicationConstants.Scope },
             { "state", values["state"] },
-            { "access_type", "offline" }
+            { "access_type", "offline" },
+            { "authorization_url", oauthUrl},
+            { "actual_redirect_uri", InvocationContext.UriInfo.AuthorizationCodeRedirectUri.ToString() },
         };
             
-        return QueryHelpers.AddQueryString(oauthUrl, parameters);
+        return QueryHelpers.AddQueryString(bridgeOauthUrl, parameters);
     }
 }
