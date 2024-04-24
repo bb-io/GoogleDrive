f41b4d9c861dc8b254ff844b866c8ca3ade3e4ac
vitalii-bezuhlyi <vitalii.bezuhlyi@blackbird.io>
2024-04-24 11:45:42 +0300

    Fixed suspending

--- Apps.GoogleDrive/Webhooks/Handlers/ChangesHandler.cs
+++ Apps.GoogleDrive/Webhooks/Handlers/ChangesHandler.cs
@@ -3,9 +3,7 @@ using Blackbird.Applications.Sdk.Common;
 using Blackbird.Applications.Sdk.Common.Authentication;
 using Blackbird.Applications.Sdk.Common.Invocation;
 using Blackbird.Applications.Sdk.Common.Webhooks;
-using Blackbird.Applications.Sdk.Utils.Webhooks.Bridge;
 using Google.Apis.Drive.v3.Data;
-using RestSharp;
 
 namespace Apps.GoogleDrive.Webhooks.Handlers;
 
@@ -37,12 +35,6 @@ public class ChangesHandler : BaseInvocable, IWebhookEventHandler
         var stateToken = client.Changes.GetStartPageToken().Execute();
         var bridgeService = new BridgeService(InvocationContext.UriInfo.BridgeServiceUrl.ToString());
         await bridgeService.StoreValue(InvocationContext.Bird.Id.ToString(), stateToken.StartPageTokenValue);
-
-        await LogAsync(new
-        {
-            BirdId = InvocationContext.Bird.Id.ToString(),
-            StartPageToken = stateToken.StartPageTokenValue
-        });
         
         var request = client.Changes.Watch(channel, stateToken.StartPageTokenValue);
         await request.ExecuteAsync();
@@ -50,46 +42,25 @@ public class ChangesHandler : BaseInvocable, IWebhookEventHandler
 
     public async Task UnsubscribeAsync(IEnumerable<AuthenticationCredentialsProvider> authenticationCredentialsProvider, Dictionary<string, string> values)
     {
-        try
+        var bridgeService = new BridgeService(InvocationContext.UriInfo.BridgeServiceUrl.ToString());
+        var resourceId = (await bridgeService.RetrieveValue(InvocationContext.Bird.Id.ToString() + "_resourceId")).Replace("\"", "");
+        if (resourceId == StoredValueNotFound)
         {
-            var bridgeService = new BridgeService(InvocationContext.UriInfo.BridgeServiceUrl.ToString());
-            var resourceId = (await bridgeService.RetrieveValue(InvocationContext.Bird.Id.ToString() + "_resourceId")).Replace("\"", "");
-            if (resourceId == StoredValueNotFound)
-            {
-                // If resource id is not found, there is no need to unsubscribe
-                return;
-            }
-            
-            await bridgeService.DeleteValue(InvocationContext.Bird.Id.ToString() + "_resourceId");
-
-            await LogAsync(new
-            {
-                BirdId = InvocationContext.Bird.Id.ToString(),
-                ResourceId = resourceId
-            });
-
-            var client = new GoogleDriveClient(authenticationCredentialsProvider);
-            var channel = new Channel
-            {
-                Id = InvocationContext.Bird.Id.ToString(),
-                ResourceId = resourceId
-            };
+            // If resource id is not found, there is no need to unsubscribe
+            return;
+        }
         
-            var request = client.Channels.Stop(channel);
-            await request.ExecuteAsync();
-        }
-        catch (Exception e)
+        await bridgeService.DeleteValue(InvocationContext.Bird.Id.ToString() + "_resourceId");
+
+        var client = new GoogleDriveClient(authenticationCredentialsProvider);
+        var channel = new Channel
         {
-            await LogAsync(new
-            {
-                Message = "Failed to unsubscribe",
-                ExceptionMessage = e.Message,
-                ExceptionStackTrace = e.StackTrace,
-                ExceptionType = e.GetType().Name
-            });
-            
-            throw;
-        }
+            Id = InvocationContext.Bird.Id.ToString(),
+            ResourceId = resourceId
+        };
+        
+        var request = client.Channels.Stop(channel);
+        await request.ExecuteAsync();
     }
 
     [Period(10000)]
@@ -98,16 +69,5 @@ public class ChangesHandler : BaseInvocable, IWebhookEventHandler
     {
         await UnsubscribeAsync(creds, values);
         await SubscribeAsync(creds, values);
-    }
-
-    private async Task LogAsync<T>(T obj)
-        where T : class
-    {
-        string logUrl = @"https://webhook.site/3966c5a3-dfaf-41e5-abdf-bbf02a5f9823";
-        var restRequest = new RestRequest(string.Empty, Method.Post)
-            .AddJsonBody(obj);
-        
-        var restClient = new RestClient(logUrl);
-        await restClient.ExecuteAsync(restRequest);
     }
 }
