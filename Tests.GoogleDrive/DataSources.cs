e83a1410daf01616540ad896a65b3d8004f36dc2
RiabushenkoA <ariabushenko@blackbird.io>
2025-05-21 18:25:28 +0300

    Update in handler to ignore recycle bin files

--- Tests.GoogleDrive/DataSources.cs
+++ Tests.GoogleDrive/DataSources.cs
@@ -1,4 +1,6 @@
-﻿using GoogleDriveTests.Base;
+﻿using Apps.GoogleDrive.DataSourceHandler;
+using Blackbird.Applications.Sdk.Common.Dynamic;
+using GoogleDriveTests.Base;
 
 namespace Tests.GoogleDrive
 {
@@ -6,12 +8,18 @@ namespace Tests.GoogleDrive
     public class DataSources : TestBase
     {
         [TestMethod]
-        public async Task DictionaryDataHandlerReturnsValues()
+        public async Task FileDataHandlerReturnsValues()
         {
+            var handler = new FileDataHandler(InvocationContext);
 
+            var response =  handler.GetData(new DataSourceContext { SearchString= "original" });
+
+            foreach (var file in response)
+            {
+                Console.WriteLine($"{file.Key} - {file.Value}");
+            }
+            Assert.IsNotNull(response);
         }
-
-
 
     }
 }
