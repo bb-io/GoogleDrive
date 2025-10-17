3ae2407f380252ce7b0db1068c6a91d7b07e231d
ce-nist <cecilia.nistal@blackbird.io>
2025-10-17 18:29:56 -0300

    Adjust action response

--- Apps.GoogleDrive/Models/Folder/GetFolderByIdResponse.cs
+++ Apps.GoogleDrive/Models/Folder/GetFolderByIdResponse.cs
@@ -7,11 +7,5 @@ public class GetFolderByIdResponse
 {
         [Display("Folder info")]
         public FolderInfo FolderInfo { get; set; } 
-
-        [Display("Parent folder ID")]
-        public string? ParentFolderId { get; set; }
-
-        [Display("Found")]
-        public bool Found { get; set; }
     
 }
