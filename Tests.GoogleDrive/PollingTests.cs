94d2b2319876e5f8493ff323a61d435fb11507cc
Anton Fesenko <tohafesenkolovep@gmail.com>
2026-03-18 18:58:50 +0200

    use file-scoped namespace in PollingTests

--- Tests.GoogleDrive/PollingTests.cs
+++ Tests.GoogleDrive/PollingTests.cs
@@ -4,67 +4,64 @@ using Apps.GoogleDrive.Polling.Models.Memory;
 using Blackbird.Applications.Sdk.Common.Polling;
 using GoogleDriveTests.Base;
 
-namespace Tests.GoogleDrive
+namespace Tests.GoogleDrive;
+
+[TestClass]
+public class PollingTests : TestBase
 {
-    [TestClass]
-    public class PollingTests : TestBase
+    [TestMethod]
+    public async Task OnFileCreated_ReturnsCreatedFiles()
     {
-        [TestMethod]
-        public async Task OnFileCreated_ReturnsCreatedFiles()
+        var polling = new PollingList(InvocationContext);
+
+        var lastInteraction = DateTime.UtcNow.AddHours(-2);
+        var memory = new DateMemory { LastInteractionDate = lastInteraction };
+
+        var pollingRequest = new PollingEventRequest<DateMemory>
         {
-            var polling = new PollingList(InvocationContext);
+            Memory = memory,
+            PollingTime = DateTime.UtcNow
+        };
 
-            var lastInteraction = DateTime.UtcNow.AddHours(-2);
-            var memory = new DateMemory { LastInteractionDate = lastInteraction };
-
-            var pollingRequest = new PollingEventRequest<DateMemory>
-            {
-                Memory = memory,
-                PollingTime = DateTime.UtcNow
-            };
-
-            var filter = new OnFileCreatedRequest
-            {
-                FolderId = "1ZgCDIk5R2IDhe2i5uEKeWPROHSnKAj8z",
-                IncludeSubfolders = true,
-                MaxSubfolderLevel = 2
-            };
-
-            var result = await polling.OnFileCreated(pollingRequest, filter);
-            var json = Newtonsoft.Json.JsonConvert.SerializeObject(result, Newtonsoft.Json.Formatting.Indented);
-            Console.WriteLine(json);
-            Assert.IsNotNull(result);
-        }
-
-        [TestMethod]
-        public async Task OnFileUpdated_IsSuccess()
+        var filter = new OnFileCreatedRequest
         {
-            var polling = new PollingList(InvocationContext);
+            FolderId = "1ZgCDIk5R2IDhe2i5uEKeWPROHSnKAj8z",
+            IncludeSubfolders = true,
+            MaxSubfolderLevel = 2
+        };
 
-            var lastInteraction = DateTime.UtcNow.AddHours(-1);
+        var result = await polling.OnFileCreated(pollingRequest, filter);
+        var json = Newtonsoft.Json.JsonConvert.SerializeObject(result, Newtonsoft.Json.Formatting.Indented);
+        Console.WriteLine(json);
+        Assert.IsNotNull(result);
+    }
 
-            var memory = new DateMemory
-            {
-                LastInteractionDate = lastInteraction
-            };
+    [TestMethod]
+    public async Task OnFileUpdated_IsSuccess()
+    {
+        var polling = new PollingList(InvocationContext);
 
-            var pollingRequest = new PollingEventRequest<DateMemory>
-            {
-                Memory = memory,
-                PollingTime = DateTime.UtcNow
-            };
-            var filter = new OnFileUpdateRequest
-            {
-                FolderId = "1ZgCDIk5R2IDhe2i5uEKeWPROHSnKAj8z",
-                IncludeSubfolders = true,
-                MaxSubfolderLevel = 2,
-                FileId = "1fEBv06mjz2lt2ffkIZUi62jiAJ_p0qoRUGB_QyAfymA"
-            };
-            var result = await polling.OnFileUpdated(filter, pollingRequest);
+        var lastInteraction = DateTime.UtcNow.AddHours(-2);
 
-            var json = Newtonsoft.Json.JsonConvert.SerializeObject(result, Newtonsoft.Json.Formatting.Indented);
-            Console.WriteLine(json);
-            Assert.IsNotNull(result);
-        }
+        var memory = new DateMemory
+        {
+            LastInteractionDate = lastInteraction
+        };
+
+        var pollingRequest = new PollingEventRequest<DateMemory>
+        {
+            Memory = memory,
+            PollingTime = DateTime.UtcNow
+        };
+        var filter = new OnFileUpdateRequest
+        {
+            FolderId = "1ZgCDIk5R2IDhe2i5uEKeWPROHSnKAj8z",
+            IncludeSubfolders = true,
+        };
+        var result = await polling.OnFileUpdated(filter, pollingRequest);
+
+        var json = Newtonsoft.Json.JsonConvert.SerializeObject(result, Newtonsoft.Json.Formatting.Indented);
+        Console.WriteLine(json);
+        Assert.IsNotNull(result);
     }
 }
