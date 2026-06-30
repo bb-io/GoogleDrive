149c89a87ea4898ba6b9c1b3c079c9c4e54ff0cf
ce-nistal <cecilia.nistal@blackbird.io>
2026-06-30 14:34:57 -0300

    Bump version

--- Apps.GoogleDrive/Models/Storage/Responses/GetFileCommentsResponse.cs
+++ Apps.GoogleDrive/Models/Storage/Responses/GetFileCommentsResponse.cs
@@ -36,7 +36,7 @@ namespace Apps.GoogleDrive.Models.Storage.Responses
         public string ID { get; set; }
         public string Content { get; set; }
         public string Author { get; set; }
-        public string Action { get; set; }
+        public string? Action { get; set; }
 
         [Display("Created time")]
         public DateTime? CreatedTime { get; set; }
